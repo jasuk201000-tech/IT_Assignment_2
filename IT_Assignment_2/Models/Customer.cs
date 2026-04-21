22d962e868dc9b3fa1b47fe1f4e2c4a0c119ebbb
jasuk201000-tech <jasuk201000@gmail.com>
2026-04-21 16:31:16 +1000

    creating the customer class, similar structure to the staff one but with a loyalty points system

---
+++ IT_Assignment_2/Models/Customer.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml.Linq;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.ListView;
+
+namespace IT_Assignment_2.Models
+{
+    internal class Customer
+    {
+        // implement an ID, first name and last name, email, loyalty points balance, and computed properties for full name and loyalty status
+
+        public int CustomerId { get; set; }
+        public string FirstName { get; set; } = null!;
+        public string LastName { get; set; } = null!; 
+        public string Email { get; set; } = string.Empty;
+        public int LoyaltyPoints { get; set; } = 0;
+
+        public string CustomerName => $"{FirstName} {LastName}";
+
+        public string LoyaltyStatus
+        {
+            get
+            {
+                if (LoyaltyPoints >= 1000)
+                    return "Gold";
+                else if (LoyaltyPoints >= 500)
+                    return "Silver";
+                else if (LoyaltyPoints >= 100)
+                    return "Bronze";
+                else
+                    return "Standard";
+            }
+        }
+    }
+}
