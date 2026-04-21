c407fa9450a02e1706632ff16f773e697537de6a
jasuk201000-tech <jasuk201000@gmail.com>
2026-04-21 17:46:15 +1000

    sizing helpers file

---
+++ IT_Assignment_2/Helpers/ProductSizing.cs
@@ -0,0 +1,23 @@
+﻿namespace AmaneRetailPOS.Constants;
+
+public static class SizeOptions
+{
+    private static readonly Dictionary<string, List<string>> _sizesByCategory = new()
+    {
+        { "Clothing", new() { "XS", "S", "M", "L", "XL" } },
+        { "Shoes", new() { "6", "7", "8", "9", "10" } },
+        { "Accessories", new() { "One Size" } }
+    };
+
+    public static List<string> GetSizesForCategory(string category)
+    {
+        return _sizesByCategory.TryGetValue(category, out var sizes)
+            ? sizes
+            : new List<string>();
+    }
+
+    public static bool IsValidSize(string category, string size)
+    {
+        return GetSizesForCategory(category).Contains(size);
+    }
+}
