51a4d169fcfb59e70f55d1b168df21dbb3ce881a
jasuk201000-tech <jasuk201000@gmail.com>
2026-04-26 06:57:57 +1000

    implemented PlaceOrder, GetAll, GetById, GetbyStaff, GetByCustomer, and GetByDateRange

---
+++ IT_Assignment_2/Services/OrderServices.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using IT_Assignment_2.Models;
+using IT_Assignment_2.Data;
+
+namespace IT_Assignment_2.Services
+{
+    public static class OrderServices
+    {
+        // implement PlaceOrder, GetAll, GetById, GetbyStaff, GetByCustomer, and GetByDateRange
+
+        // place order method
+        public static List<Order> GetAll() =>
+                CsvHelper.LoadOrders();
+
+        // get order by id method
+        public static Order? GetById(Guid orderId) =>   
+            CsvHelper.LoadOrders()
+                .FirstOrDefault(o => o.OrderId == orderId);
+
+
+        // get order by staff method
+        public static List<Order> GetByStaff(Guid staffId) =>
+            CsvHelper.LoadOrders()
+                .Where(o => o.StaffId == staffId)
+                .ToList();
+
+        // get order by customer method
+
+        public static List<Order> GetByCustomer(Guid customerId) =>
+            CsvHelper.LoadOrders()
+                .Where(o => o.CustomerId == customerId)
+                .ToList();
+
+        // get order by date range method
+
+        public static List<Order> GetByDateRange(DateTime startDate, DateTime endDate) =>
+            CsvHelper.LoadOrders()
+                .Where(o => o.CreatedAt >= startDate && o.CreatedAt <= endDate)
+                .ToList();
+
+
+
+    }
+}
