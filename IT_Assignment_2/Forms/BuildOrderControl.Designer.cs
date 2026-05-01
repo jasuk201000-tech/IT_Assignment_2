fd8b565d6cf8027c8b9078d51dd5ab47230cc67b
jasuk201000-tech <jasuk201000@gmail.com>
2026-05-02 07:23:19 +1000

    finsihed UI

--- IT_Assignment_2/Forms/BuildOrderControl.Designer.cs
+++ IT_Assignment_2/Forms/BuildOrderControl.Designer.cs
@@ -30,32 +30,54 @@
         {
             Panel orderPnl;
             Panel mainPnl;
+            panel7 = new Panel();
+            removeBtn = new Button();
             ordersumTxt = new TextBox();
             panel6 = new Panel();
+            comboBox3 = new ComboBox();
+            stock4 = new TextBox();
             price4 = new TextBox();
             item4 = new TextBox();
             panel5 = new Panel();
+            comboBox4 = new ComboBox();
+            stock5 = new TextBox();
             price6 = new TextBox();
             item5 = new TextBox();
             panel4 = new Panel();
+            comboBox5 = new ComboBox();
+            textBox1 = new TextBox();
             price7 = new TextBox();
             item6 = new TextBox();
             panel3 = new Panel();
+            comboBox1 = new ComboBox();
+            stock3 = new TextBox();
             price3 = new TextBox();
             item3 = new TextBox();
             panel2 = new Panel();
+            sizeCombo = new ComboBox();
+            stock2 = new TextBox();
             price2 = new TextBox();
             item2 = new TextBox();
             panel1 = new Panel();
+            comboBox2 = new ComboBox();
             stock1 = new TextBox();
             price1 = new TextBox();
             item1 = new TextBox();
             categoryComboBox = new ComboBox();
             searchboxTxt = new TextBox();
             searchTxt = new TextBox();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            textBox2 = new TextBox();
+            discountTxt = new TextBox();
+            applydiscountBtn = new Button();
+            chargeBtn = new Button();
             orderPnl = new Panel();
             mainPnl = new Panel();
             orderPnl.SuspendLayout();
+            panel7.SuspendLayout();
             mainPnl.SuspendLayout();
             panel6.SuspendLayout();
             panel5.SuspendLayout();
@@ -68,11 +90,40 @@
             // orderPnl
             // 
             orderPnl.BackColor = SystemColors.ControlLight;
+            orderPnl.Controls.Add(chargeBtn);
+            orderPnl.Controls.Add(applydiscountBtn);
+            orderPnl.Controls.Add(discountTxt);
+            orderPnl.Controls.Add(textBox2);
+            orderPnl.Controls.Add(label4);
+            orderPnl.Controls.Add(label3);
+            orderPnl.Controls.Add(label2);
+            orderPnl.Controls.Add(label1);
+            orderPnl.Controls.Add(panel7);
             orderPnl.Controls.Add(ordersumTxt);
             orderPnl.Location = new Point(648, 0);
             orderPnl.Name = "orderPnl";
             orderPnl.Size = new Size(415, 556);
             orderPnl.TabIndex = 0;
+            // 
+            // panel7
+            // 
+            panel7.BackColor = Color.White;
+            panel7.Controls.Add(removeBtn);
+            panel7.Location = new Point(59, 67);
+            panel7.Name = "panel7";
+            panel7.Size = new Size(317, 166);
+            panel7.TabIndex = 2;
+            // 
+            // removeBtn
+            // 
+            removeBtn.BackColor = Color.White;
+            removeBtn.Font = new Font("Segoe UI", 15F);
+            removeBtn.Location = new Point(271, 29);
+            removeBtn.Name = "removeBtn";
+            removeBtn.Size = new Size(29, 34);
+            removeBtn.TabIndex = 0;
+            removeBtn.Text = "ⓧ";
+            removeBtn.UseVisualStyleBackColor = false;
             // 
             // ordersumTxt
             // 
@@ -106,12 +157,35 @@
             // 
             panel6.BackColor = Color.White;
             panel6.BorderStyle = BorderStyle.FixedSingle;
+            panel6.Controls.Add(comboBox3);
+            panel6.Controls.Add(stock4);
             panel6.Controls.Add(price4);
             panel6.Controls.Add(item4);
             panel6.Location = new Point(20, 270);
             panel6.Name = "panel6";
-            panel6.Size = new Size(164, 100);
+            panel6.Size = new Size(164, 118);
             panel6.TabIndex = 8;
+            // 
+            // comboBox3
+            // 
+            comboBox3.Font = new Font("Cambria", 8F);
+            comboBox3.FormattingEnabled = true;
+            comboBox3.Location = new Point(25, 76);
+            comboBox3.Name = "comboBox3";
+            comboBox3.Size = new Size(100, 20);
+            comboBox3.TabIndex = 7;
+            comboBox3.Text = "choose size";
+            // 
+            // stock4
+            // 
+            stock4.BorderStyle = BorderStyle.None;
+            stock4.Font = new Font("Cambria", 8F);
+            stock4.Location = new Point(25, 57);
+            stock4.Name = "stock4";
+            stock4.Size = new Size(100, 13);
+            stock4.TabIndex = 5;
+            stock4.Text = "low in stock";
+            stock4.TextAlign = HorizontalAlignment.Center;
             // 
             // price4
             // 
@@ -140,12 +214,35 @@
             // 
             panel5.BackColor = Color.White;
             panel5.BorderStyle = BorderStyle.FixedSingle;
+            panel5.Controls.Add(comboBox4);
+            panel5.Controls.Add(stock5);
             panel5.Controls.Add(price6);
             panel5.Controls.Add(item5);
             panel5.Location = new Point(246, 270);
             panel5.Name = "panel5";
-            panel5.Size = new Size(164, 100);
+            panel5.Size = new Size(164, 118);
             panel5.TabIndex = 7;
+            // 
+            // comboBox4
+            // 
+            comboBox4.Font = new Font("Cambria", 8F);
+            comboBox4.FormattingEnabled = true;
+            comboBox4.Location = new Point(32, 76);
+            comboBox4.Name = "comboBox4";
+            comboBox4.Size = new Size(100, 20);
+            comboBox4.TabIndex = 8;
+            comboBox4.Text = "choose size";
+            // 
+            // stock5
+            // 
+            stock5.BorderStyle = BorderStyle.None;
+            stock5.Font = new Font("Cambria", 8F);
+            stock5.Location = new Point(32, 57);
+            stock5.Name = "stock5";
+            stock5.Size = new Size(100, 13);
+            stock5.TabIndex = 6;
+            stock5.Text = "out of stock";
+            stock5.TextAlign = HorizontalAlignment.Center;
             // 
             // price6
             // 
@@ -174,12 +271,35 @@
             // 
             panel4.BackColor = Color.White;
             panel4.BorderStyle = BorderStyle.FixedSingle;
+            panel4.Controls.Add(comboBox5);
+            panel4.Controls.Add(textBox1);
             panel4.Controls.Add(price7);
             panel4.Controls.Add(item6);
             panel4.Location = new Point(464, 270);
             panel4.Name = "panel4";
-            panel4.Size = new Size(164, 100);
+            panel4.Size = new Size(164, 118);
             panel4.TabIndex = 6;
+            // 
+            // comboBox5
+            // 
+            comboBox5.Font = new Font("Cambria", 8F);
+            comboBox5.FormattingEnabled = true;
+            comboBox5.Location = new Point(32, 76);
+            comboBox5.Name = "comboBox5";
+            comboBox5.Size = new Size(100, 20);
+            comboBox5.TabIndex = 9;
+            comboBox5.Text = "choose size";
+            // 
+            // textBox1
+            // 
+            textBox1.BorderStyle = BorderStyle.None;
+            textBox1.Font = new Font("Cambria", 8F);
+            textBox1.Location = new Point(32, 57);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(100, 13);
+            textBox1.TabIndex = 7;
+            textBox1.Text = "10 in stock";
+            textBox1.TextAlign = HorizontalAlignment.Center;
             // 
             // price7
             // 
@@ -208,12 +328,35 @@
             // 
             panel3.BackColor = Color.White;
             panel3.BorderStyle = BorderStyle.FixedSingle;
+            panel3.Controls.Add(comboBox1);
+            panel3.Controls.Add(stock3);
             panel3.Controls.Add(price3);
             panel3.Controls.Add(item3);
             panel3.Location = new Point(464, 115);
             panel3.Name = "panel3";
-            panel3.Size = new Size(164, 100);
+            panel3.Size = new Size(164, 115);
             panel3.TabIndex = 5;
+            // 
+            // comboBox1
+            // 
+            comboBox1.Font = new Font("Cambria", 8F);
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(32, 76);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(100, 20);
+            comboBox1.TabIndex = 5;
+            comboBox1.Text = "choose size";
+            // 
+            // stock3
+            // 
+            stock3.BorderStyle = BorderStyle.None;
+            stock3.Font = new Font("Cambria", 8F);
+            stock3.Location = new Point(32, 57);
+            stock3.Name = "stock3";
+            stock3.Size = new Size(100, 13);
+            stock3.TabIndex = 4;
+            stock3.Text = "25 in stock";
+            stock3.TextAlign = HorizontalAlignment.Center;
             // 
             // price3
             // 
@@ -242,12 +385,35 @@
             // 
             panel2.BackColor = Color.White;
             panel2.BorderStyle = BorderStyle.FixedSingle;
+            panel2.Controls.Add(sizeCombo);
+            panel2.Controls.Add(stock2);
             panel2.Controls.Add(price2);
             panel2.Controls.Add(item2);
             panel2.Location = new Point(246, 115);
             panel2.Name = "panel2";
-            panel2.Size = new Size(164, 100);
+            panel2.Size = new Size(164, 115);
             panel2.TabIndex = 4;
+            // 
+            // sizeCombo
+            // 
+            sizeCombo.Font = new Font("Cambria", 8F);
+            sizeCombo.FormattingEnabled = true;
+            sizeCombo.Location = new Point(32, 76);
+            sizeCombo.Name = "sizeCombo";
+            sizeCombo.Size = new Size(100, 20);
+            sizeCombo.TabIndex = 4;
+            sizeCombo.Text = "choose size";
+            // 
+            // stock2
+            // 
+            stock2.BorderStyle = BorderStyle.None;
+            stock2.Font = new Font("Cambria", 8F);
+            stock2.Location = new Point(32, 57);
+            stock2.Name = "stock2";
+            stock2.Size = new Size(100, 13);
+            stock2.TabIndex = 3;
+            stock2.Text = "9 in stock";
+            stock2.TextAlign = HorizontalAlignment.Center;
             // 
             // price2
             // 
@@ -276,13 +442,24 @@
             // 
             panel1.BackColor = Color.White;
             panel1.BorderStyle = BorderStyle.FixedSingle;
+            panel1.Controls.Add(comboBox2);
             panel1.Controls.Add(stock1);
             panel1.Controls.Add(price1);
             panel1.Controls.Add(item1);
             panel1.Location = new Point(20, 115);
             panel1.Name = "panel1";
-            panel1.Size = new Size(164, 100);
+            panel1.Size = new Size(164, 115);
             panel1.TabIndex = 3;
+            // 
+            // comboBox2
+            // 
+            comboBox2.Font = new Font("Cambria", 8F);
+            comboBox2.FormattingEnabled = true;
+            comboBox2.Location = new Point(25, 76);
+            comboBox2.Name = "comboBox2";
+            comboBox2.Size = new Size(100, 20);
+            comboBox2.TabIndex = 6;
+            comboBox2.Text = "choose size";
             // 
             // stock1
             // 
@@ -351,6 +528,87 @@
             searchTxt.TabIndex = 0;
             searchTxt.Text = "select products";
             // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Cambria", 10F);
+            label1.Location = new Point(59, 251);
+            label1.Name = "label1";
+            label1.Size = new Size(57, 16);
+            label1.TabIndex = 3;
+            label1.Text = "subtotal";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Cambria", 10F);
+            label2.Location = new Point(59, 284);
+            label2.Name = "label2";
+            label2.Size = new Size(109, 16);
+            label2.TabIndex = 4;
+            label2.Text = "discount amount";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Cambria", 10F);
+            label3.Location = new Point(59, 309);
+            label3.Name = "label3";
+            label3.Size = new Size(26, 16);
+            label3.TabIndex = 5;
+            label3.Text = "tax";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Cambria", 15F);
+            label4.Location = new Point(59, 331);
+            label4.Name = "label4";
+            label4.Size = new Size(68, 23);
+            label4.TabIndex = 6;
+            label4.Text = "TOTAL";
+            // 
+            // textBox2
+            // 
+            textBox2.BackColor = Color.FromArgb(224, 224, 224);
+            textBox2.BorderStyle = BorderStyle.None;
+            textBox2.Font = new Font("Cambria", 9F);
+            textBox2.Location = new Point(59, 376);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(100, 15);
+            textBox2.TabIndex = 7;
+            textBox2.Text = "discount code";
+            // 
+            // discountTxt
+            // 
+            discountTxt.Location = new Point(59, 406);
+            discountTxt.Name = "discountTxt";
+            discountTxt.Size = new Size(118, 23);
+            discountTxt.TabIndex = 8;
+            discountTxt.Text = "AMANE10";
+            // 
+            // applydiscountBtn
+            // 
+            applydiscountBtn.Font = new Font("Cambria", 9F);
+            applydiscountBtn.Location = new Point(226, 406);
+            applydiscountBtn.Name = "applydiscountBtn";
+            applydiscountBtn.Size = new Size(75, 23);
+            applydiscountBtn.TabIndex = 9;
+            applydiscountBtn.Text = "apply";
+            applydiscountBtn.UseVisualStyleBackColor = true;
+            // 
+            // chargeBtn
+            // 
+            chargeBtn.BackColor = Color.RosyBrown;
+            chargeBtn.Font = new Font("Cambria", 10F);
+            chargeBtn.Location = new Point(59, 449);
+            chargeBtn.Name = "chargeBtn";
+            chargeBtn.Size = new Size(242, 41);
+            chargeBtn.TabIndex = 10;
+            chargeBtn.Text = "Charge";
+            chargeBtn.TextAlign = ContentAlignment.MiddleLeft;
+            chargeBtn.UseVisualStyleBackColor = false;
+            // 
             // BuildOrderControl
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
@@ -361,6 +619,7 @@
             Size = new Size(1061, 523);
             orderPnl.ResumeLayout(false);
             orderPnl.PerformLayout();
+            panel7.ResumeLayout(false);
             mainPnl.ResumeLayout(false);
             mainPnl.PerformLayout();
             panel6.ResumeLayout(false);
@@ -381,27 +640,48 @@
         #endregion
 
         private TextBox ordersumTxt;
-        private TextBox searchboxTxt;
+        private Panel panel7;
+        private Button removeBtn;
         private TextBox searchTxt;
-        private Panel panel1;
+        private TextBox searchboxTxt;
         private ComboBox categoryComboBox;
-        private Panel panel6;
-        private Panel panel5;
-        private Panel panel4;
-        private Panel panel3;
-        private Panel panel2;
-        private TextBox item1;
-        private TextBox item4;
-        private TextBox item5;
-        private TextBox item6;
-        private TextBox item3;
-        private TextBox item2;
-        private TextBox price4;
-        private TextBox price3;
-        private TextBox price2;
-        private TextBox price1;
-        private TextBox price6;
-        private TextBox price7;
+        private Panel panel1;
+        private ComboBox comboBox2;
         private TextBox stock1;
+        private TextBox price1;
+        private TextBox item1;
+        private Panel panel2;
+        private ComboBox sizeCombo;
+        private TextBox stock2;
+        private TextBox price2;
+        private TextBox item2;
+        private Panel panel3;
+        private ComboBox comboBox1;
+        private TextBox stock3;
+        private TextBox price3;
+        private TextBox item3;
+        private Panel panel4;
+        private ComboBox comboBox5;
+        private TextBox textBox1;
+        private TextBox price7;
+        private TextBox item6;
+        private Panel panel5;
+        private ComboBox comboBox4;
+        private TextBox stock5;
+        private TextBox price6;
+        private TextBox item5;
+        private Panel panel6;
+        private ComboBox comboBox3;
+        private TextBox stock4;
+        private TextBox price4;
+        private TextBox item4;
+        private Label label4;
+        private Label label3;
+        private Label label2;
+        private Label label1;
+        private TextBox textBox2;
+        private Button chargeBtn;
+        private Button applydiscountBtn;
+        private TextBox discountTxt;
     }
 }
